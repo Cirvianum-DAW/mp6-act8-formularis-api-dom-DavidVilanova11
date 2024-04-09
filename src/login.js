6e0351adfdad689b4de73e4f4c7c3db77ee4dfa5
daw25 <navjashan.singh@cirvianum.cat>
2024-04-09 17:03:38 +0200

    Add comment to get form data

--- src/login.js
+++ src/login.js
@@ -2,6 +2,7 @@ document
     .getElementById('login')
     .addEventListener('submit', (e)=> {
         e.preventDefault();
+        // Obtenir les dades del formulari
         const email = document.getElementById('email').value;
         const password = document.getElementById('password').value;
         login(email, password);
