d3f176657ad3e330182d0cc6b54e82bb7201fdc2
daw25 <navjashan.singh@cirvianum.cat>
2024-04-09 16:59:55 +0200

    Add styles for form and weather results

---
+++ src/login.js
@@ -0,0 +1,9 @@
+document
+    .getElementById('login')
+    .addEventListener('submit', (e)=> {
+        e.preventDefault();
+        const email = document.getElementById('email').value;
+        const password = document.getElementById('password').value;
+        login(email, password);
+    }
+);
