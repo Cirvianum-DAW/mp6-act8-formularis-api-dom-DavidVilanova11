d3e503438e4b5527cc51cdd9611cd07ba13f3471
daw25 <navjashan.singh@cirvianum.cat>
2024-04-10 16:22:13 +0200

    sessionStorage

--- src/login.js
+++ src/login.js
@@ -7,12 +7,13 @@ document.addEventListener('DOMContentLoaded', (event) => {
             const email = document.getElementById('email').value;
             const password = document.getElementById('password').value;
             const dataNaixement = document.getElementById('dataNaixement').value;
+            const dniNumber = document.getElementById('dniNumber').value;
             const termsAccepted = document.getElementById('terms').checked;
 
             // Form field validation
 
             // Check that they are filled
-            if (!email || !password) {
+            if (!email || !password || !dataNaixement || !dniNumber) {
                 alert('You must fill all fields');
                 return;
             }
@@ -37,8 +38,43 @@ document.addEventListener('DOMContentLoaded', (event) => {
 
             const today = new Date();
             const birthDate = new Date(dataNaixement);
+            const age = today.getFullYear() - birthDate.getFullYear();
+
+            // 1 any a milisegons(formula)
+            // 1 any = 365 dies * 24 hores * 60 minuts * 60 segons * 1000 milisegons  
+            // teniendo en cuenta los bisiestos necesitamos un 365,25
+            // 1 any = 365,25 dies * 24 hores * 60 minuts * 60 segons * 1000 milisegonsqqqqqqqq     
 
             //login(email, password);
-        }
-    );
-});
+            if (age < 18) {
+                alert('You must be at least 18 years old');
+                return;
+            }
+
+            const dniRegex = /^[0-9]{8}[A-Z]$/;
+            if (!dniRegex.test(dniNumber)) {
+                alert('The DNI is not valid');
+                return;
+            }   
+
+            // Send data to the server
+            
+            const formData = {
+                email,
+                password,
+                dataNaixement,
+                dniNumber,
+            };
+
+            sessionStorage.setItem('formData', JSON.stringify(formData));
+
+            const storedFormData = JSON.parse(sessionStorage.getItem('formData'));
+            console.log(storedFormData);
+
+           // window.location.href = 'meteo.html';
+
+            }
+
+        );
+}
+);
